bde1f56308add3625fb91a246dc52412d1bb6962
Priyanshu-777 <singhpriyansu09@gmail.com>
2025-12-31 20:21:52 +0530

    initial code for cf tracker

--- src/tracker.py
+++ src/tracker.py
@@ -9,11 +9,7 @@ def get_user_info(handle):
     
     print(response2,"\n")
 
-    """"json.dumps() → converts Python dict to formatted JSON string
-
-    indent=4 → adds spacing & indentation
-
-    Makes nested data easy to read"""
+    
 
 
 def main():
