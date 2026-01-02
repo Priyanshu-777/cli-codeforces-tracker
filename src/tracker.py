7555e57b45739d18180ad9cafe55b3b6087f8d78
Priyanshu-777 <singhpriyansu09@gmail.com>
2026-01-02 20:26:39 +0530

    get user submissions

--- src/tracker.py
+++ src/tracker.py
@@ -16,17 +16,37 @@ def get_user_info(handle):
     print(f"Rating: {user_info.get('rating', 'Unrated')}")
     print(f"Max Rating: {user_info.get("maxRating", "Unrated")}")  
     print(f"Contribution: {user_info["contribution"]}")  
-    print('\n')                
+                   
     
 
-    
+def get_user_submissions(handle):
+    url =  f"https://codeforces.com/api/user.status?handle={handle}"
+    response = requests.get(url).json()
+    #print(json.dumps(response,indent=4))
+
+    submissions_list= response['result']
+    solved = set()
+
+    for submission in submissions_list:
+        if submission['verdict']== "OK":        # solution accepted by cf
+            problem = submission['problem']
+            solved_problem_id = (problem.get("contestId"), problem.get("index"))
+
+            solved.add(solved_problem_id)
+
+    print(f"Number of problem solved: {len(solved)}\n")
+
+
+
 def header():
-    print(f'\n######################## Codeforces Tracker ########################\n')
+    print(f'\n########################## Codeforces Tracker ##########################\n')
 
 def main():
     handle= input("Enter codeforce handle: ")
     header()
     get_user_info(handle)
+    get_user_submissions(handle)
+
 
 
 
